63cd6f6a9786f0ff96b37693aec34dcab3dfeff9
Shankar Kulumani <shanks.k@gmail.com>
2017-07-29 17:00:04 -0400

    Update setup repo

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # add the correct remote repositories
-git_repo="git@github.com:skulumani/latex_writeups.git"
-bb_repo="git@bitbucket.org:shankarkulumani/latex_writeups.git"
+git_repo="git@github.com:skulumani/dissertation.git"
+bb_repo="git@bitbucket.org:shankarkulumani/dissertation.git"
 
 printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
 printf "Github: $git_repo\n"
