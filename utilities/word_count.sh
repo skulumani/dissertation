4048cfe51086036f6240a193ae297867f0bcd385
Shankar Kulumani <shanks.k@gmail.com>
2018-06-30 10:56:58 -0400

    Now loop over git commits

--- utilities/word_count.sh
+++ utilities/word_count.sh
@@ -2,25 +2,23 @@
 
 INPUT_FILE="dissertation.tex"
 OUTPUT_FILE="word_count.csv"
-
-COUNTS="$(texcount -1 -merge -q ${INPUT_FILE})"
-
-WORDS_TEXT="$(awk -F'[+]' '{print $1}' <<< ${COUNTS})"
-WORDS_HEADER="$(awk -F'[+]' '{print $2}' <<< ${COUNTS})"
-WORDS_CAPTION="$(awk -F'[+|(]' '{print $3}' <<< ${COUNTS})"
-NUM_HEADER="$(awk -F'[(|/]' '{print $2}' <<< ${COUNTS})"
-NUM_FLOAT="$(awk -F'[/]' '{print $2}' <<< ${COUNTS})"
-NUM_MATH_INLINE="$(awk -F'[/]' '{print $3}' <<< ${COUNTS})"
-NUM_MATH_DISPLAY="$(awk -F'[/|)]' '{print $4}' <<< ${COUNTS})"
-
-echo "Words in text: ${WORDS_TEXT}"
-echo "Words in headers: ${WORDS_HEADER}"
-echo "Words in cations: ${WORDS_CAPTION}"
-echo "Number of headers: ${NUM_HEADER}"
-echo "Number of floats: ${NUM_FLOAT}"
-echo "Number of math inline: ${NUM_MATH_INLINE}"
-echo "Number of math display: ${NUM_MATH_DISPLAY}"
+BRANCH="system_model"
 
 echo "COMMIT,DATE,WORDS_TEXT,WORDS_HEADER,WORDS_CAPTION,NUM_HEADER,NUM_FLOAT,NUM_MATH_INLINE,NUM_MATH_DISPLAY" > ${OUTPUT_FILE}
 
-echo "${WORDS_TEXT},${WORDS_HEADER},${WORDS_CAPTION// /},${NUM_HEADER},${NUM_FLOAT},${NUM_MATH_INLINE},${NUM_MATH_DISPLAY}" >> ${OUTPUT_FILE}
+for COMMIT in $(git rev-list -n 10 --reverse ${BRANCH})
+do
+    COUNTS="$(git show $COMMIT:$INPUT_FILE 2>/dev/null | texcount -1 -merge -q -)"
+    
+    DATE="$(git log -1 --date=iso --pretty=format:%ad $COMMIT)"
+    WORDS_TEXT="$(awk -F'[+]' '{print $1}' <<< ${COUNTS})"
+    WORDS_HEADER="$(awk -F'[+]' '{print $2}' <<< ${COUNTS})"
+    WORDS_CAPTION="$(awk -F'[+|(]' '{print $3}' <<< ${COUNTS})"
+    NUM_HEADER="$(awk -F'[(|/]' '{print $2}' <<< ${COUNTS})"
+    NUM_FLOAT="$(awk -F'[/]' '{print $2}' <<< ${COUNTS})"
+    NUM_MATH_INLINE="$(awk -F'[/]' '{print $3}' <<< ${COUNTS})"
+    NUM_MATH_DISPLAY="$(awk -F'[/|)]' '{print $4}' <<< ${COUNTS})"
+    echo "${COMMIT},${DATE},${WORDS_TEXT},${WORDS_HEADER},${WORDS_CAPTION// /},${NUM_HEADER},${NUM_FLOAT},${NUM_MATH_INLINE},${NUM_MATH_DISPLAY}" >> ${OUTPUT_FILE}
+done
+
+
